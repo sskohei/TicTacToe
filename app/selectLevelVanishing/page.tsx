30f6868791b9e189a9588ccb3bb0ee6bdf8a8741
sskohei <k.kuro.2007@gmail.com>
2026-01-28 19:11:55 +0900

    難易度選択(消える三目並べ)のページを追加

--- app/selectLevelVanishing/page.tsx
+++ app/selectLevelVanishing/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button"
 
-export default function SelectLevelTic(){
+export default function SelectLevelVanishing(){
     return(
         <div className="min-h-screen flex flex-col items-center justify-center gap-6">
             <h1 className="text-3xl font-bold">難易度を選択</h1>
