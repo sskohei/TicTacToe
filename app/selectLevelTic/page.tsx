1881dd9f00a47ec650cbc54b4764c3271ab523c0
sskohei <k.kuro.2007@gmail.com>
2026-01-28 19:10:19 +0900

    難易度選択(三目並べ)のページを追加

---
+++ app/selectLevelTic/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home(){
+    return(
+        <div className="min-h-screen flex flex-col items-center justify-center gap-6">
+            <h1 className="text-3xl font-bold">難易度を選択</h1>
+            <div className="flex gap-2">
+              <Button>
+                <a href="/">Easy</a>
+              </Button>
+              <Button>
+                <a href="/">Normal</a>
+              </Button>
+              <Button>
+                <a href="/">Hard</a>
+              </Button>
+            </div>
+        </div>
+    )
+}
+
